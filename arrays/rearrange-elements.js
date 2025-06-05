92d5e00c8005a6071909bf6d307d0427d5344879
Sreejith Narayanankutty <writeto.sreejithn@gmail.com>
2025-06-05 11:35:58 +0530

    rearrange elements better approach

--- arrays/rearrange-elements.js
+++ arrays/rearrange-elements.js
@@ -1,18 +1,34 @@
-const rearrangeElements = (nums)=> {
-    let positivenumbers=[], negativenumbers= []
+// const rearrangeElements = (nums)=> {
+//     let positivenumbers=[], negativenumbers= []
+//     for(let i=0;i<nums.length;i++) {
+//         if(nums[i]>0) {
+//             positivenumbers.push(nums[i])
+//         } else {
+//             negativenumbers.push(nums[i])
+//         }
+//     }
+
+//     for(let i=0;i<nums.length;i+=2) {
+//         nums[i] = positivenumbers[i/2]
+//         nums[i+1] = negativenumbers[i/2]
+//     }
+//     return nums
+// }
+
+//console.log(rearrangeElements([3,1,-2,-5,2,-4]))
+
+const rearrageElements= (nums)=> {
+    let posindex=0, negIndex=1
+    let res= new Array(nums.length)
     for(let i=0;i<nums.length;i++) {
-        if(nums[i]>0) {
-            positivenumbers.push(nums[i])
+        if(nums[i]<0) {
+            res[negIndex] = nums[i]
+            negIndex+=2
         } else {
-            negativenumbers.push(nums[i])
+            res[posindex] = nums[i]
+            posindex+=2
         }
     }
-
-    for(let i=0;i<nums.length;i+=2) {
-        nums[i] = positivenumbers[i/2]
-        nums[i+1] = negativenumbers[i/2]
-    }
-    return nums
+    return res
 }
-
-console.log(rearrangeElements([3,1,-2,-5,2,-4]))
+console.log(rearrageElements([3,1,-2,-5,2,-4]))
